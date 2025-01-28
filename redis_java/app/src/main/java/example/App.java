33ef8ba50a20dd6e94c2425ff5e2961463037d0d
butgo87 <b317201@nate.com>
2025-01-28 14:11:13 +0900

    redis_java 수정중

--- redis_java/app/src/main/java/example/App.java
+++ redis_java/app/src/main/java/example/App.java
@@ -5,12 +5,14 @@ import redis.clients.jedis.DefaultJedisClientConfig;
 import redis.clients.jedis.Jedis;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
+
+    private final Jedis jedisConnection;
+
+    public App() {
+        this.jedisConnection = createJedisConnection();
     }
 
-    public static void main(String[] args) {
-
+    private Jedis createJedisConnection() {
         Dotenv dotenv = Dotenv.configure()
                 .directory("../../")
                 .ignoreIfMalformed()
@@ -18,7 +20,7 @@ public class App {
                 .load();
 
         String host = dotenv.get("REDIS_HOST");
-        int port = Integer.valueOf(dotenv.get("REDIS_PORT"));
+        int port = Integer.parseInt(dotenv.get("REDIS_PORT"));
         String username = dotenv.get("REDIS_USERNAME");
         String password = dotenv.get("REDIS_PASSWORD");
         System.out.printf("host: %s%n", host);
@@ -26,14 +28,23 @@ public class App {
         System.out.printf("username: %s%n", username);
         System.out.printf("password: %s%n", password);
 
-        DefaultJedisClientConfig defaultJedisClientConfig = DefaultJedisClientConfig.builder()
+        DefaultJedisClientConfig jedisClientConfig = DefaultJedisClientConfig.builder()
                 .user(username)
                 .password(password)
                 .build();
 
-        try (Jedis jedis = new Jedis(host, port, defaultJedisClientConfig)) {
-            String ping = jedis.ping();
-            System.out.printf("ping: %s%n", ping);
-        }
+        return new Jedis(host, port, jedisClientConfig); // Jedis 인스턴스 생성
+    }
+
+    public void ping() {
+        String ping = jedisConnection.ping();
+        System.out.printf("ping: %s%n", ping);
+    }
+
+    public static void main(String[] args) {
+
+        App app = new App();
+
+        app.ping();
     }
 }
