dc6a840b83798648bb09cc5b278c4648175f5685
sjbyun <b317201@nate.com>
2025-02-01 18:30:26 +0900

    sqlite python push

--- rdb_python/README.md
+++ rdb_python/README.md
@@ -1,3 +1,12 @@
+### 파이썬의 표준 스타일 권고안 PEP8
+	1. 함수와 변수의 이름에 스네이크 케이스를 권장함
+	2. 사용자 클래스 이름은 카멜케이스를 권장함
+
+### 모듈설치
+	pip install python-dotenv
+
+
+
 python
 venv
 python3 -m pip install --user -U virtualenv
--- rdb_python/sqlite.py
+++ rdb_python/sqlite.py
@@ -1,84 +1,64 @@
 import sqlite3
+import os
+from dotenv import load_dotenv
 
-def create_connection(db_file):
-    conn = None
-    try:
-        conn = sqlite3.connect(db_file)
-        return conn
-    except sqlite3.Error as e:
-        print(e)
-    return conn
+conn = None
 
-def create_table(conn):
+
+def create_connection():
+
+    load_dotenv("../.env")
+    sqlitedb = os.getenv("SQLITEDB")
+    print("sqlitedb : " + sqlitedb)
+    global conn
     try:
-        sql_create_table = """ CREATE TABLE IF NOT EXISTS users (
-                                    id integer PRIMARY KEY,
-                                    name text NOT NULL,
-                                    age integer
-                                ); """
-        conn.execute(sql_create_table)
+        conn = sqlite3.connect("../" + sqlitedb)
+        print("Connection is established: Database is created on file " + sqlitedb)
     except sqlite3.Error as e:
         print(e)
 
-def insert_user(conn, user):
-    sql = ''' INSERT INTO users(name, age)
-              VALUES(?,?) '''
-    cur = conn.cursor()
-    cur.execute(sql, user)
-    conn.commit()
-    return cur.lastrowid
+def create_table():
+    try:
+        sql = """ CREATE TABLE IF NOT EXISTS user (
+                        user_id varchar(10)
+                      , first_name varchar(50)
+                      , last_name varchar(50)
+                      , age integer
+                      , CONSTRAINT pk_user PRIMARY KEY (user_id)
+                  ); """
+        print("sql: " + sql)
+        conn.execute(sql)
+    except sqlite3.Error as e:
+        print(e)
+        raise e
 
-def select_all_users(conn):
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM users")
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-
-def update_user(conn, user):
-    sql = ''' UPDATE users
-              SET name = ? ,
-                  age = ?
-              WHERE id = ?'''
-    cur = conn.cursor()
-    cur.execute(sql, user)
-    conn.commit()
-
-def delete_user(conn, id):
-    sql = 'DELETE FROM users WHERE id=?'
-    cur = conn.cursor()
-    cur.execute(sql, (id,))
-    conn.commit()
+def read_data():
+    try:
+        sql = """ SELECT
+                          user_id
+                        , first_name
+                        , last_name
+                        , age
+                    FROM
+                       user """
+        print("sql: " + sql)
+        cur = conn.cursor()
+        cur.execute(sql)
+        rows = cur.fetchall()
+        print("rows: ", rows)
+        for row in rows:
+            print("row: " + row)
+    except sqlite3.Error as e:
+        print(e)
+        raise e
 
 def main():
-    database = r"test.db"
+    print("Hello, World!")
+    create_connection()
 
-    conn = create_connection(database)
-    if conn is not None:
-        create_table(conn)
+    create_table()
+    read_data()
 
-        user_1 = ('John', 25)
-        user_2 = ('Anna', 30)
 
-        insert_user(conn, user_1)
-        insert_user(conn, user_2)
-
-        print("Users before update:")
-        select_all_users(conn)
-
-        update_user(conn, ('John Doe', 26, 1))
-
-        print("Users after update:")
-        select_all_users(conn)
-
-        delete_user(conn, 2)
-
-        print("Users after deletion:")
-        select_all_users(conn)
-
-        conn.close()
-    else:
-        print("Error! Cannot create the database connection.")
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
