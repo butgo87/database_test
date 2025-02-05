29111f9333cf93f68667ef15fc6f391d07a4ab31
sjbyun <b317201@nate.com>
2025-02-05 20:04:45 +0900

    jpa

--- php/pdo.php
+++ php/pdo.php
@@ -1,20 +1,11 @@
 <?php
 class DbConn
 {
-    private static $instance = null;
-    private $pdo;
-
-    // public static function getConnection() {
-    //     if(self::$instance == null){
-    //         self::$instance = new Database();
-    //     }
-    //     return self::$instance;
-    // }
 
     public static function getConnection() {
-        if(self::$instance == null) {
-            self::$instance = new DbConn;
+        if(self::$instance == null){
+            self::$instance = new Database();
         }
-        return self::$pdo;
+        return self::$instance;
     }
 }
